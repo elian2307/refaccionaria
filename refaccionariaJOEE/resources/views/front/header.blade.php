6843b306ffee146a8144cfca3219ccbd86c288fc
Erickgalleta18 <erickgarciamangorolo@gmail.com>
2026-03-09 13:09:47 -0600

    avance
    avance erick

---
+++ refaccionariaJOEE/resources/views/front/header.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+
+    <div class="logo">JOEE Mechanics</div>
+
+    <div class="menu">
+        <a href="#">Home</a>
+        <a href="#">Repairs</a>
+        <a href="#">Sale</a>
+        <a href="#">Contact</a>
+        <a href="#">Log In</a>
+    </div>
+
+</nav>
